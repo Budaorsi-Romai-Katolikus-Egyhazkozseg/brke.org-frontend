e0eac90e0627e31e348b2479860c20c278aa2530
István Donkó <istvan.donko@gmail.com>
2022-10-30 05:09:14 +0100

    Change redirection mechanism to page meta property

--- pages/_.vue
+++ pages/_.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { onBeforeMount } from "vue";
-
-onBeforeMount(() =>
-  navigateTo({ path: window.location.hash.slice(1), replace: true })
-);
+definePageMeta({
+  redirect: (to) => {
+    const { _: path, ...query } = to.query;
+    return { path, query, replace: true };
+  },
+});
 </script>
