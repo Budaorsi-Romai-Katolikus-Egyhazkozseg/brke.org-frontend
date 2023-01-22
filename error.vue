51db8996df546edcf8e8363fd0149fe59d74f7b9
István Donkó <istvan.donko@gmail.com>
2023-01-22 23:24:21 +0100

    Add a custom error handler page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <h1>Hiba ({{ error.statusCode }})</h1>
+    <p>A keresett oldal betöltése közben hiba történt.</p>
+    <a href="/">
+      <button type="button" class="btn btn-primary">
+        Vissza a főoldalra
+      </button>
+    </a>
+  </NuxtLayout>
+</template>
+
+<script>
+export default { props: { error: Object } }
+</script>
