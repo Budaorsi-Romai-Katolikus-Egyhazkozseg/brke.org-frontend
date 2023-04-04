3c35f0dc403c5801beb0b46512a38dfc6eb27d97
István Donkó <istvan.donko@gmail.com>
2023-04-04 16:10:42 +0200

    Add plausible analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@ import { cmsURL } from './utils/constants'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [
+        {
+          defer: true,
+          'data-domain': 'brke.org',
+          src: 'https://analytics.brke.donko.hu/js/script.js',
+        },
+      ],
+    },
+  },
   css: ['~/assets/scss/main.scss', 'bootstrap-icons/font/bootstrap-icons.css'],
   modules: ['@nuxtjs/strapi', 'nuxt-purgecss'],
   purgecss: { enabled: false, safelist: ['collapsing'] },
