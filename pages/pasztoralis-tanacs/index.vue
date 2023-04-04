f8711e5079d7cdd3ad3d79c5d9e966ba0d848d62
István Donkó <istvan.donko@gmail.com>
2023-04-04 03:55:13 +0200

    Add description for the upcoming council election

---
+++ pages/pasztoralis-tanacs/index.vue
@@ -0,0 +1,155 @@
+<script setup>
+import { titlePostfix } from '~/utils/constants.ts'
+
+useHead({ title: `Pasztorális Tanács - ${titlePostfix}` })
+</script>
+
+<template>
+  <h1 class="card-title">Pasztorális Tanács</h1>
+
+  <p>
+    Ahogy már a hirdetésekben hallottuk, megkezdődött a budaörsi
+    plébániaközösség <i>pasztorális tanácsának</i> (régi nevén:
+    képviselőtestület) <i>újraválasztási folyamata</i>. A folyamat hátteréről,
+    alapvető menetéről, a „pasztorális tanács” és a „képviselőtestület”
+    kifejezések viszonyáról Márió atya írt az Élünk újság 2023 húsvéti számában.
+  </p>
+  <p>
+    Amikor 2021-ben Püspök atya megkérte Márió atyát, hogy bár a fő feladata a
+    főiskolai tanítás marad, de emellett – egyelőre plébániai kormányzóként, de
+    teljes plébánosi jogkörrel – mégis vállalja el a budaörsi plébánia
+    vezetését, akkor azt is kifejezte, hogy megyéspüspökként minden megoldást
+    támogat, amivel a plébániaközösségi feladatokat – az Egyház gondolkodásának
+    megfelelő módon – szét lehet osztani több személy közt. Azt is kifejezte,
+    hogy szeretné, ha a budaörsi egyházközség
+    <i>a világiak sokrétű szolgálatával működő plébánia egyfajta példájává</i>
+    válna.
+  </p>
+  <p>
+    Ezek alapján kértük a Szabályzattól való részbeni eltérés engedélyezését a
+    mostani testület-megújításra, amit meg is kaptunk Püspök atyától. Az
+    eltérések indítékairól, céljairól szintén az Élünk-cikkben olvashatunk.
+    Ebben a leírásban a folyamat gyakorlati magyarázatát adjuk.
+  </p>
+  <p>
+    A jelenlegi fázisban megnyílt a jelentkezés lehetősége a budaörsi római
+    katolikus egyházközség különböző <i>munkacsoportjaiba</i>, illetve ha
+    vállalod a jelöltséget a választásra, akkor a hozzájuk kapcsolódó
+    <i>bizottságokba</i>.
+  </p>
+
+  <h2>A munkacsoportok</h2>
+  <p>
+    A <b>munkacsoportok</b> nem képezik a létrehozandó pasztorális tanács
+    részét, hanem az egész plébániaközösség minden felnőtt tagja számra
+    meghívást jelentenek a közösség életében való kifejezett, meghatározott
+    feladatvállalásra. Ilyen módon tehát a munkacsoportok megalakítása a
+    pasztorális tanács választásának apropóján kitűzött önálló közösségi célnak
+    is tekinthető. Ugyanakkor mégis van köze a választáshoz, mert bizottsági
+    tagjelöltnek – ami már szoros kapcsolatban van a pasztorális tanáccsal –
+    akkor jelentkezhetsz, ha valamelyik munkacsoportban – a későbbi választás
+    eredményétől függetlenül – feladatot vállaltál.
+  </p>
+  <p>
+    Legfeljebb 3 munkacsoportban vállalhatsz szerepet ezzel a jelentkezéssel,
+    amennyiben elmúltál 18 éves, és valamilyen módon a budaörsi
+    plébániaközösséghez kapcsolódsz. (A budaörsi lakcím nem szükséges feltétel.)
+    A munkacsoportba való jelentkezés készséget jelent részedről arra, hogy ha
+    ezen a téren feladat jelentkezik a plébániaközösségben, akkor megszólítható
+    vagy a segítségre. Természetesen más feladatokban is segíthetsz, a 3-as
+    limit az intézményes részvételre vonatkozik, és az erők koncentrálásában,
+    tisztázásában segít.
+  </p>
+  <p>
+    A munkacsoportoknak nincs külön vezetésük, üléseik: ezeket a feladatokat a
+    hozzájuk rendelt bizottságok látják el. A munkacsoport tehát akár úgy is
+    tekinthető, mint olyanok név- és címlistája, akiket a megfelelő bizottság
+    szükség esetén megkérdezhet, hogy tudnak-e a konkrét kitűzött feladatban
+    segíteni. Az is előfordulhat, hogy a bizottság megkérdezi a munkacsoportok
+    tagjait, hogy feladatvégzésük során az emberek között milyen tanulságokat
+    tapasztaltak, amelyeket kiértékelve fejleszteni lehetne a plébániaközösség
+    szolgálatát, ha az elgondolást a plébános atya a bizottság javaslata és a
+    pasztorális tanács véleményének meghallgatása után megfelelőnek ítéli.
+  </p>
+  <p>
+    A bizottság – megalakulása után – meg fogja kérdezni tőled, hogy a
+    munkacsoport leírásánál felsorolt sok feladat közül pontosabban melyikre is
+    érzed magad alkalmasnak. Tehát nem az összes lehetséges feladatra kell
+    vállalkoznod! Inkább ha a felsoroltak között látsz akár csak egy olyat,
+    amelyben szívesen segítenél, jelentkezz a megfelelő (legfeljebb 3)
+    munkacsoportba! Lehet, hogy már régóta segítesz az adott feladat
+    ellátásában; ha így van, ezt ezúton is köszönjük, és kérünk, hogy akkor
+    mindenképp jelentkezz a munkacsoportba, egyrészt hogy egy listában
+    láthassunk mindenkit, aki azon a téren tevékeny vagy az szeretne lenni,
+    másrészt mert ez esetben valószínűleg szükségünk van tapasztalatodra.
+  </p>
+
+  <h2>A bizottságok</h2>
+  <p>
+    <b>Bizottságba</b> a hívek szavazatai alapján kerülhetsz be, de legfeljebb
+    csak egybe. Emiatt fontos a jelentkezéseden belüli sorrend felállítása:
+    ezzel azt határozod meg, hogy – ha több bizottságba is jelentkeztél
+    tagjelöltnek – esetleges többszörös megválasztásod esetén melyik
+    bizottságban végzett munka lenne a legtesthezállóbb számodra.
+  </p>
+  <p>
+    A bizottságok feladata az alájuk tartozó munkacsoportoknál felsorolt
+    feladatok áttekintése, összeegyeztetése, fontosságuk súlyozása, ellátásuk
+    megszervezése, a tervezés és a koordináció. Tehát ha szívesen részt
+    vállalnál az adott területen a <i>szervezésben, tervezésben</i>, akkor
+    jelentkezz bizottsági tagjelöltnek is, mert ez a fajta tevékenység ott
+    történik (nem a munkacsoportokban).
+  </p>
+  <p>
+    A bizottságok 4–6 főből állnak majd (a pontos megválasztható létszámot az
+    egyes bizottságokra vonatkozóan majd a szavazólap tartalmazza, annak
+    függvényében is, hogy mennyien jelentkeztek bizottsági tagjelöltnek). A
+    plébániát vezető lelkipásztornak joga van hivatalból olyan tagot is
+    delegálni a bizottságokba (korlátozott számban), aki nem szerepel a
+    jelöltlistán, de megbízatása, munkaköre alapján fontos a részvétele az adott
+    grémiumban.
+  </p>
+  <p>
+    A bizottságoknak bizonyos időközönként találkozókat, üléseket kell
+    tartaniuk, hogy az ügyeket kézben tarthassák, munkacsoportjaik életét,
+    feladatellátását megszervezzék, és javaslatokat tehessenek a pasztorális
+    tanácsnak a hozzájuk tartozó ügyekben. A bizottságok működési rendje eltérő
+    lehet a hozzájuk tartozó témák mennyisége és sürgőssége függvényében, de egy
+    önkéntesmunka kereteit igyekszünk nem meghaladni velük (esetleges
+    „kampányszerű” feladatok idejét kivéve). Évente 1-2 alkalommal az összes
+    bizottság összehívható együttes találkozóra.
+  </p>
+  <p>
+    Fontos, hogy lehetőleg minél többen jelentkezzetek bizottsági
+    tagjelöltségre, hogy legyen kik közül választani! A bizottságoknak nem
+    minden tagja lesz a pasztorális tanács tagja. A bizottságok maguk választják
+    majd meg vezetőjüket, aki képviseli őket ebben a tanácsban.
+  </p>
+  <p>
+    Egy különleges státuszú bizottság a Gazdasági bizottság. Ennek megválasztási
+    módjáról a szabályok külön rendelkeznek, ezért ezt mi sem külső választás
+    útján hozzuk létre, hanem a pasztorális tanács az előírásoknak megfelelően
+    saját kebelén belül választja meg.
+  </p>
+
+  <h2>A pasztorális tanács</h2>
+  <p>
+    A pasztorális (lelkipásztori) tanács létrehozását maga az Egyházi
+    Törvénykönyv írja elő. Tagjai a plébánián a megyéspüspök megbízásából
+    szolgálatot ellátó papok (esetünkben jelenleg Márió és János atyák), a
+    hivatalból tagok (például a kántor, a katolikus iskola képviselője, a
+    katekéták – hitoktatók – képviselője stb.), a bizottságok megválasztott
+    vezetői, és azok, akiknek részvételét – korlátozott létszámban – esetleg a
+    plébániát vezető lelkipásztor fontosnak tartja ezen a szinten.
+  </p>
+  <p>
+    A pasztorális tanács nem parlamentáris és nem döntéshozó testület, hanem a
+    plébániaközösség életéért felelős lelkipásztor tanácsadó testülete. A
+    bizottságok vezetőinek megválasztást követően alakul meg, megválasztja
+    tagjai közül az egyházközség világi elnökét, majd esküt tesz. Üléseit
+    nagyjából havi-kéthavi rendszerességgel tartja, a szükségletek függvényében.
+    Legfőbb küldetése segíteni a plébániát vezető lelkipásztort a
+    plébániaközösség sokrétű életének összehangolásában, egységbe rendezésében.
+    Mérlegeli és összhangba hozza az egyes bizottságok cselekvési javaslatait.
+  </p>
+</template>
