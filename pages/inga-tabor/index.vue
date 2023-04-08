8f96514db4a87c167757c12721c7245708f27056
István Donkó <istvan.donko@gmail.com>
2023-04-08 15:26:47 +0200

    Add a placeholder page for Inga-Tábor registration

---
+++ pages/inga-tabor/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { titlePostfix } from '~/utils/constants.ts'
+
+useHead({ title: `Inga-Tábor - ${titlePostfix}` })
+</script>
+
+<template>
+  <h1 class="card-title">Inga-tábor</h1>
+
+  <p>
+    <i>A jelentkezési lehetőség hamarosan megnyílik, kis türelmet kérünk!</i>
+  </p>
+</template>
