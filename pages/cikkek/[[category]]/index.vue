0108fa871fd51e7f3d4992a0ecba0155d7eba121
István Donkó <istvan.donko@gmail.com>
2023-08-14 09:19:39 +0200

    Add category filtering to articles

--- pages/cikkek/[[category]]/index.vue
+++ pages/cikkek/[[category]]/index.vue
@@ -7,8 +7,22 @@ const { find } = useStrapi()
 
 const page = computed(() => (route.query.oldal ? Number(route.query.oldal) : 1))
 
+const getCategories = async () =>
+  await find('categories', {
+    populate: '*',
+    sort: 'name:asc',
+  })
+const categories = ref(await getCategories())
+
+const currentCategory = computed(() =>
+  categories.value.data.find((c) => c.attributes.slug === route.params.category)
+)
+
 const getArticles = async () =>
   await find('articles', {
+    ...(currentCategory.value
+      ? { filters: { category: currentCategory.value.id } }
+      : {}),
     pagination: { pageSize: 8, page: page.value },
     populate: '*',
     sort: 'date:desc',
@@ -34,7 +48,45 @@ watch(
 
 <template>
   <div>
-    <h2>Összes</h2>
+    <div class="d-flex justify-content-between align-items-center py-2">
+      <h2>{{ currentCategory?.attributes?.name || 'Összes' }}</h2>
+      <div class="dropdown">
+        <button
+          type="button"
+          :class="[
+            'btn',
+            'btn-primary',
+            'btn-sm',
+            'dropdown-toggle',
+            'd-flex',
+            'justify-content-around',
+            'align-items-center',
+          ]"
+          :style="{ width: '9em' }"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="bi-filter"></i>
+          Szűrés
+        </button>
+        <ul class="dropdown-menu" :style="{ minWidth: 'unset', width: '8em' }">
+          <li>
+            <NuxtLink class="dropdown-item" :to="{ params: { category: '' } }">
+              Összes
+            </NuxtLink>
+          </li>
+          <li><hr class="dropdown-divider" /></li>
+          <li v-for="category in categories.data">
+            <NuxtLink
+              class="dropdown-item"
+              :to="{ params: { category: category.attributes.slug } }"
+            >
+              {{ category.attributes.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
     <div class="mb-4">
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
         <ArticleCard
--- pages/cikkek/[category]/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h2>{{ $route.params.category }}</h2>
-  </div>
-</template>
