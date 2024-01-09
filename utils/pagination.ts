2e3336ed677ac05a3caaf509ec45953dcd7272d3
István Donkó <istvan.donko@gmail.com>
2024-01-09 17:23:34 +0100

    Fix pagination button sorting

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,9 +1,9 @@
 export const getPageItems = (
   center: number,
   length: number,
-  count: number
+  count: number,
 ): number[] =>
   Array.from({ length }, (_, i) => i + 1)
     .sort((a, b) => Math.abs(a - center) - Math.abs(b - center))
     .slice(0, count)
-    .sort();
+    .sort((a, b) => a - b)
