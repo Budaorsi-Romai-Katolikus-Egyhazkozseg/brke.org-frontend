07d4fba81862056247354447fdf37083e49d316c
István Donkó <istvan.donko@gmail.com>
2024-01-14 11:17:39 +0100

    Fix responsive table class and update code styling

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,24 +1,26 @@
-import MarkdownIt from "markdown-it";
+import MarkdownIt from 'markdown-it'
 
 export const md = new MarkdownIt({
   breaks: true,
   html: true,
   linkify: true,
   typographer: true,
-});
+})
 
 // Set default class for tables
-md.renderer.rules.table_open = () => '<table class="table table-responsive">';
+md.renderer.rules.table_open = () =>
+  ['<div class="table-responsive">', '<table class="table">'].join('\n')
+md.renderer.rules.table_close = () => ['</table>', '</div>'].join('\n')
 
 // Set default class for images and add base URL if the path is relative
-const defaultImageRenderer = (md.renderer.rules.image as Function)
+const defaultImageRenderer = md.renderer.rules.image as Function
 md.renderer.rules.image = (tokens, idx, options, env, self) => {
   tokens[idx].attrJoin('class', 'img-fluid')
 
   const src = tokens[idx].attrGet('src')
   if (src?.startsWith('/')) {
-    tokens[idx].attrSet('src', `https://cms.brke.org${src}`)
+    tokens[idx].attrSet('src', cmsURL + src)
   }
 
   return defaultImageRenderer(tokens, idx, options, env, self)
-};
+}
