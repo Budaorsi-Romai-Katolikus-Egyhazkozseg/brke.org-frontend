89d2c0b1b77d4e4a7019e46cbd67649f6e92eac6
István Donkó <istvan.donko@gmail.com>
2024-02-10 01:06:03 +0100

    Handle CMS connection failure, show placeholders

--- pages/index.vue
+++ pages/index.vue
@@ -5,27 +5,46 @@ useHead({ title: `Kezdőlap - ${titlePostfix}` })
 
 const { find } = useStrapi()
 
-const homepage = (await find('homepage')).data.attributes
+const homepage = ref(undefined)
+const articles = ref(undefined)
 
-const articles = await find('articles', {
-  filters: { current: true },
-  populate: '*',
-  sort: 'date:desc',
-})
+try {
+  // Fetch homepage markdown content
+  find('homepage').then((result) => {
+    homepage.value = result.data
+  })
+
+  // Fetch current articles
+  find('articles', {
+    filters: { current: true },
+    populate: '*',
+    sort: 'date:desc',
+  }).then((result) => {
+    articles.value = result.data
+  })
+} catch (error) {
+  console.error(error)
+}
 </script>
 
 <template>
   <div class="mb-3 d-flex flex-column gap-2">
     <h1>Kezdőlap</h1>
 
-    <MarkdownContent :source="homepage.content" />
+    <MarkdownContent v-if="homepage" :source="homepage.attributes.content" />
+    <div v-else class="container placeholder-glow">
+      <span class="placeholder col-7"></span>
+      <br />
+      <span class="placeholder col-4"></span>
+    </div>
 
     <EventList />
     <div class="container">
       <h2>Aktuális / Kiemelt cikkek</h2>
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
         <ArticleCard
-          v-for="article in articles.data"
+          v-if="articles"
+          v-for="article in articles"
           :category="article.attributes.category.data.attributes"
           :title="article.attributes.title"
           :imageUrl="
@@ -36,6 +55,26 @@ const articles = await find('articles', {
           :teaser="article.attributes.teaser"
           :slug="article.attributes.slug"
         />
+        <div v-else class="col">
+          <div class="card placeholder-glow">
+            <div
+              class="card-img-top placeholder"
+              :style="{ height: '15rem', background: 'gray' }"
+            />
+            <div class="card-body">
+              <h5 class="card-title placeholder-glow">
+                <span class="placeholder col-6"></span>
+              </h5>
+              <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-8"></span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
